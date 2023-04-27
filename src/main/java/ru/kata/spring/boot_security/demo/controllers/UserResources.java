92ee9a6190c472b477a5bb8230c602e58ccaaa80
ev.shpako <shpakovev95@gmail.com>
2023-04-27 22:26:39 +0300

    ver.3 Front + add validator for REST controller

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserResources.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserResources.java
@@ -1,20 +1,40 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.Util.UserValidator;
 import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.service.UserServiceImpl;
+
+
+
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 
 @RestController
 public class UserResources {
 
     private final UserServiceImpl userService;
+    private final UserValidator userValidator;
+
 
 
     @Autowired
-    public UserResources(UserServiceImpl userService) {
+    public UserResources(UserServiceImpl userService, UserValidator userValidator) {
         this.userService = userService;
+        this.userValidator = userValidator;
+    }
 
+    @InitBinder("user")
+    protected void initBinder(WebDataBinder binder) {
+        binder.addValidators(userValidator);
     }
 
     @GetMapping("/user/{userId}")
@@ -22,11 +42,28 @@ public class UserResources {
         return userService.getUserById(userId);
     }
 
-    @PostMapping("/user/edit")
-    public boolean updateEdit(@RequestBody User user) {
-        return userService.update(user);
+//    @PostMapping("/user/edit")
+//    public boolean updateEdit(@RequestBody User user) {
+//        return userService.update(user);
+//
+//    }
 
+    @PostMapping("/user/edit")
+    public ResponseEntity<Boolean> updateEdit(@Validated @RequestBody User user, BindingResult result) {
+        if (result.hasErrors()) {
+            System.out.println(ResponseEntity.badRequest().body(result.getAllErrors()));
+            return ResponseEntity.badRequest().body(false);
+        }
+
+        boolean success = userService.update(user);
+        if (success) {
+            return ResponseEntity.ok(true);
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(false);
+        }
     }
+
+
 
     @DeleteMapping("/user/delete/{userId}")
     public void delete(@PathVariable long userId) {
